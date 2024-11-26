87f81dc6aa113f9d7ecb4fcfb82e4d5deacb3f0c
anubhootipandey <anubhootipandey05@gmail.com>
2024-11-26 13:53:03 +0530

    added pallindromechecker

---
+++ PallindromChecker.js
@@ -0,0 +1,8 @@
+function palindrome(str) {
+  const normalizedStr = str.replace(/[^A-Za-z0-9]/g, "").toLowerCase();
+  const reversedStr = normalizedStr.split("").reverse().join("");
+
+  return normalizedStr === reversedStr;
+}
+
+palindrome("eye");
